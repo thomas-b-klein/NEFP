6cc63a4ad948df8ef48262f346f9a6777731bd02
thomas-b-klein <thomasklein@vt.edu>
2023-06-27 15:22:33 -0400

    added useful functions to utility.py

--- utility.py
+++ utility.py
@@ -9,3 +9,9 @@ def make_implicit(func):
         return func(x,*args)-y
 
     return implicit_func
+
+# Useful Functions
+
+def within(x, bounds, include_bounds = True):
+    if include_bounds: return True if min(bounds) <= x <= max(bounds) else False
+    if not include_bounds: return True if min(bounds) < x < max(bounds) else False
