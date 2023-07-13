c148bf2ad91836baaeae0e5daeb96c1b2cddbbff
thomas-b-klein <thomasklein@vt.edu>
2023-07-13 13:26:37 -0400

    added nozzle class

--- nozzle.py
+++ nozzle.py
@@ -1,15 +1,18 @@
 from utility import *
+from plotting import *
 from ifr import ISENTROPIC as ISEN
 from nsr import NORMAL as NORM
 # from osr import OBLIQUE as OBLQ
 
-import matplotlib.pyplot as plt
 import numpy as np
 
 class GEOMETRY():
     def __init__(self, aeas, gamma = 1.4):
+        if aeas < 1.0: raise ValueError('A/A* must be greater or equal to 1.') 
+
         self.aeas = aeas
         self.gamma = gamma
+
         self.exit_sub = ISEN.from_aas(aeas, gamma = gamma, subsonic = True)
         self.exit_sup = ISEN.from_aas(aeas, gamma = gamma, subsonic = False)
 
@@ -19,7 +22,7 @@ class GEOMETRY():
 
     @property 
     def choked(self):
-        return self.exit_sub.pp0
+        return float(self.exit_sub.pp0)
 
     @property
     def shock_in_nozzle(self):
@@ -28,7 +31,7 @@ class GEOMETRY():
     @property
     def shock_at_exit(self):
         p2p1 = NORM(self.exit_sup.M, gamma = self.gamma).p2p1
-        return p2p1 * self.design
+        return float(p2p1 * self.design)
     
     @property
     def over_expanded(self):
@@ -36,21 +39,58 @@ class GEOMETRY():
 
     @property
     def design(self):
-        return self.exit_sup.pp0
+        return float(self.exit_sup.pp0)
 
     @property
     def under_expanded(self):
-        return (self.design, np.inf)
+        return (self.design, 0)
 
-    def find_condition(self, pbpc):
-        if pbpc >= 1.0 or pbpc <= 0.0: raise ValueError('pb/pc cannot be greater than 1 or less than 0.')
-        if within(pbpc, self.subsonic): return (0,'Subsonic')
-        elif pbpc == self.choked: return (1, 'Choked')
-        elif within(pbpc, self.shock_in_nozzle): return (2, 'Shock in Nozzle')
-        elif pbpc == self.shock_at_exit: return (3, 'Shock at Exit')
-        elif within(pbpc, self.over_expanded): return (4, 'Over-Expanded')
-        elif pbpc == self.design: return (5, 'Design')
-        elif within(pbpc, self.under_expanded): return (6, 'Under-Expanded')
-        else: 
-            categories = [self.subsonic, self.shock_in_nozzle, self.over_expanded, self.under_expanded]
-            raise ValueError('pb/pc does not fit into any of the categories:\n{}'.format(categories))
+    def ranges(self):
+        return [self.subsonic, self.shock_in_nozzle, self.over_expanded, self.under_expanded]
+    
+    def point(self):
+        return [self.choked, self.shock_at_exit, self.design]
+
+
+
+class NOZZLE():
+    def __init__(self, aeas, pbpc, gamma = 1.4):
+        if pbpc <= 0.0: raise ValueError('pb/pc value must be greater than 0.')
+        elif pbpc > 1: raise ValueError('Reversed flow: pb/pc value must be less than 1.')
+        elif pbpc == 1: raise ValueError('No flow: pb/pc value must be less than 1.')
+
+        self.pbpc = pbpc
+        self.aeas = aeas
+
+        pbpc_ranges = GEOMETRY(aeas, gamma=gamma).ranges()
+        pbpc_points = GEOMETRY(aeas, gamma=gamma).points()
+        functions = ([NOZZLE.choked, NOZZLE.shock_at_exit, NOZZLE.design],
+                    [NOZZLE.subsonic, NOZZLE.shock_in_nozzle, NOZZLE.over_expanded, NOZZLE.under_expanded])
+        for func, point in zip(functions[0], pbpc_points):
+            if pbpc == point: func(self); break
+        for func, range in zip(functions[1], pbpc_ranges):
+            if within(pbpc, range): func(self); break
+
+    def subsonic(self):
+        print('subsonic')
+
+    def shock_in_nozzle(self):
+        print('sin')
+    
+    def over_expanded(self):
+        print('over')
+
+    def under_expanded(self):
+        print('under')
+
+    def choked(self):
+        print('choked')
+    
+    def shock_at_exit(self):
+        print('shock at exit')
+
+    def design(self):
+        print('design')
+
+# print(GEOMETRY(5).ranges())
+# print(NOZZLE(5,0.995))
